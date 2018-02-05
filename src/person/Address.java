3613acb34ce4f0d2ec6fe16e100d71c3479701e8
Gnanamithra <bg049380>
2018-02-05 20:26:40 +0530

    person obj

---
+++ src/person/Address.java
@@ -0,0 +1,13 @@
+package person_utility;
+
+/**
+ * @author Benison
+ * Stores the address information.
+ */
+public class Address {
+    public Address() {}
+	
+    private int doorNo;
+    private String street;
+    private String town;
+}
