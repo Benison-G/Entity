9a2c386c32c00bde45e8610754b3242cbe90e7ac
Gnanamithra <bg049380>
2018-02-05 22:59:26 +0530

    update

--- src/person/Address.java
+++ src/person/Address.java
@@ -1,13 +1,64 @@
-package person;
-
 /**
  * @author Benison
- * Stores the address information.
+ * Contains the address information.
  */
 public class Address {
-    public Address() {}
-	
-    private int doorNo;
+	/**
+	 * Constructor of Address object.
+	 * @param doorNo The unique identifier for a door.
+	 * @param street The string representing street.
+	 * @param town The string representing the town.
+	 */
+    public Address(int doorNo, String street, String town) {
+		super();
+		this.doorNo = doorNo;
+		this.street = street;
+		this.town = town;
+	}
+
+	private int doorNo;
     private String street;
     private String town;
+    
+    /**
+     * @return The unique identifier for a door.
+     */
+	public int getDoorNo() {
+		return doorNo;
+	}
+	
+	/**
+	 * @param doorNo The unique identifier for a door.
+	 */
+	public void setDoorNo(int doorNo) {
+		this.doorNo = doorNo;
+	}
+	
+	/**
+	 * @return The string representing street.
+	 */
+	public String getStreet() {
+		return street;
+	}
+	
+	/**
+	 * @param street The string representing street.
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	/**
+	 * @return The string representing town.
+	 */
+	public String getTown() {
+		return town;
+	}
+	
+	/**
+	 * @param town The string representing town.
+	 */
+	public void setTown(String town) {
+		this.town = town;
+	}
 }
--- src/person/Person.java
+++ src/person/Person.java
@@ -1,5 +1,3 @@
-package person;
-
 import java.util.Date;
 
 enum Gender {
@@ -12,7 +10,19 @@ enum Gender {
  * Contains information regarding person.
  */
 public class Person {
-    private String firstName;
+	/**
+	 * Constructor of Person object.
+	 * @param firstName
+	 * @param lastName
+	 */
+    public Person(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		fullName = firstName +  " " + lastName;
+	}
+
+	private String firstName;
     private String lastName;
     private String fullName;
 
@@ -22,11 +32,89 @@ public class Person {
     
     private Address address;
 
-    Person (){}
-    Person (String firstName, String lastName)
-    {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        fullName = firstName + " " + lastName;
-    }
+    /**
+     * @return The first name of the person.
+     */
+    public String getFirstName() {
+		return firstName;
+	}
+    
+    /**
+     * @param firstName The first name of the person.
+     */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	/**
+	 * @return The last name of the person.
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	
+	/**
+	 * @param lastName The last name of the person.
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	/**
+	 * @return The full name of the person.
+	 */
+	public String getFullName() {
+		return fullName;
+	}
+	
+	/**
+	 * Sets the full name of the person.
+	 * @param firstName The first name of the person.
+	 * @param lastName The last name of the person.
+	 */
+	public void setFullName(String firstName, String lastName) {
+		this.fullName = firstName + " " + lastName;
+	}
+	
+	/**
+	 * @return The gender of the person.
+	 */
+	public Gender getGender() {
+		return gender;
+	}
+	
+	/**
+	 * @param gender The gender of the person.
+	 */
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	
+	/**
+	 * @return The date of birth of the person.
+	 */
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	
+	/**
+	 * @param dateOfBirth The date of birth of the person.
+	 */
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	/**
+	 * @return The address of the person.
+	 */
+	public Address getAddress() {
+		return address;
+	}
+	
+	/**
+	 * @param address The address of the person.
+	 */
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 }
