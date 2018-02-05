b472e4701191dc8cd16f14b5d5854436179cdc44
Gnanamithra <bg049380>
2018-02-05 13:04:00 +0530

    Person class update

--- src/person/Person.java
+++ src/person/Person.java
@@ -2,21 +2,26 @@ package person;
 
 import java.util.Date;
 
+import person_utility.Address;
+
 enum Gender {
-    MALE,
-    FEMALE
+    "MALE",
+    "FEMALE"
 }
 
 /**
- * @author bg049380
+ * @author Benison
  * Stores information regarding person.
  */
 public class Person {
     private String firstName;
     private String lastName;
     private String fullName;
+
     private Gender gender;
+    
     private Date dateOfBirth;
+    
     private Address address;
 
     Person (){}
